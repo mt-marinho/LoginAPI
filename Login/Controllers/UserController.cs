50e6e286b3509f68725546b3f46ac0dd06737606
Matheus Marinho <mt.mrnh.esp@gmail.com>
2024-11-22 22:40:37 -0400

    criação do projeto

---
+++ Login/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+using Login.Data;
+
+namespace Login.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ Login/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using Login.Models;
+
+namespace Login.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
